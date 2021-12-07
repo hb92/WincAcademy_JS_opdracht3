291ffe60c16bdadd95b674dea561e44630e21f73
hb92 <britt.hellemans@gmail.com>
2021-12-07 21:00:11 +0100

    Add files via upload

---
+++ JS_opdracht3.js
@@ -0,0 +1,24 @@
+const age = 18;
+const isFemale = true;
+const driverStatus = "bob";
+
+if (age >= 18) {
+	console.log("Kom er in!");
+}
+else {
+	console.log("Eruit!");
+    }
+
+if (isFemale) {
+    console.log("Hello Gurl");
+}
+else {
+    console.log("No D's allowed");
+}
+
+if (driverStatus == "bob") {
+    console.log("Rij jij maar, jij bent nuchter!")
+}
+else {
+    console.log("dit gaat niet meer lukken")
+}
